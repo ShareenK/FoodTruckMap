fe268c968dbf7ce4e6d8b000619c89aae1c01658
Hery <aeonzamaek@gmail.com>
2013-03-14 13:04:47 -0400

    added map view

--- src/com/acm/foodtruckmap/MainActivity.java
+++ src/com/acm/foodtruckmap/MainActivity.java
@@ -1,15 +1,20 @@
 package com.acm.foodtruckmap;
 
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends MapActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		MapView mapView = (MapView) findViewById(R.id.mapview);
+	    mapView.setBuiltInZoomControls(true);
 	}
 
 	@Override
@@ -17,6 +22,12 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
